cae12b7ddfcc39a43a45c7d53674860abd18a3e1
alaa-alshariff <alaa.alshariff@hotmail.com>
2023-12-20 03:02:52 -0500

    .

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -1,6 +1,6 @@
 import Born from "./born.png";
 import cartoon from "./experience.png";
-import HeroImg from "./HeroImg.jpg";
+import HeroImg from "./Heroimg.jpg";
 import github from "./github.png"
 export {
     Born, cartoon, HeroImg, github
