3b2d80c80ce13be70268017d6142f4d378807212
alaa-alshariff <alaa.alshariff@hotmail.com>
2023-12-24 20:58:22 -0500

    resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
               growth mindset, I am ready to make a meaningful contribution and
               achieve great things.
             </div>
-            <a href="https://profile.indeed.com/p/alaaa-67h4gcq" target="_blank" className="resumebtn">
+            <a href="https://1drv.ms/b/s!AgFty9D6yzPBcf90DRW6afNr2Zo?e=LWCB0L" target="_blank" className="resumebtn">
               Resume
             </a>
           </div>
