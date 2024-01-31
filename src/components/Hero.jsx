8cdac3a790b29d46295bd7355f072e62e98ea542
alaa-alshariff <alaa.alshariff@hotmail.com>
2024-01-30 23:45:37 -0500

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
               growth mindset, I am ready to make a meaningful contribution and
               achieve great things.
             </div>
-            <a href="https://1drv.ms/b/s!AgFty9D6yzPBgQ0jJBtRPubhmj2H?e=lx4jc8" target="_blank" className="resumebtn">
+            <a href="https://1drv.ms/b/s!AgFty9D6yzPBgQ7CW6UP9xoqduIU?e=cs9YQB" target="_blank" className="resumebtn">
               Resume
             </a>
           </div>
